97da4a916fc968b771d5ff6faa8185fbae719228
Gds Desenvolvimento e Tecnologia <104175823+GdsDesenvolvimentoETecnologia@users.noreply.github.com>
2022-07-24 22:54:51 -0300

    Initial Commit

---
+++ Frases do dia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Frases do dia
+//
+//  Created by Gabriel Alves on 24/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
